9d9f3346dd2134bdc0473eb3749b435a055846fd
BiteableFunds <131499753+BiteableFunds@users.noreply.github.com>
2023-12-10 11:38:57 -0500

    Update pagescript.js

--- pagescript.js
+++ pagescript.js
@@ -1,29 +1,8 @@
 var buzzer = document.getElementById("bzr")
 var buzzerdisplay = document.getElementById("bzrdisplay")
-var selectedTeam = 0
 
-function setDisplay(timestamp) {
-  var date = new Date(timestamp).toLocaleTimeString("en-US")
+function buzzerPress() {
+  let unixtimestamp = Date.now();
+  var date = new Date(unixtimestamp).toLocaleTimeString("en-US")
   buzzerdisplay.innerHTML = `buzzed in at: ${date} ${unixtimestamp}`
 }
-
-async function sendTimeData(team, timestamp) {
-  const endpoint = new URL('https://77220z28-20000.use.devtunnels.ms/teams');
-  fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-  },
-    body: JSON.stringify({
-      team: team,
-      timestamp: unixtimestamp
-    })
-}
-
-function bzrPress(team) {
-  let unixtimestamp = Date.now();
-  setDisplay(unixtimestamp);
-  sendTimeData(team, unixtimestamp);
-}
-
-buzzer.addEventListener("click", bzrPress(selectedTeam));
